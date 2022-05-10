019b024ddf7a4a32e7688a1dbb394d013cc413e2
Vitor Pouza <vhnpouza@gmail.com>
2022-05-10 00:37:02 -0300

    delete and add task button

--- src/app/kanban/board/board.component.ts
+++ src/app/kanban/board/board.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { TaskDialogComponent } from '../dialogs/task-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 import { BoardService } from '../board.service';
-import { Board } from '../board.model';
+import { Board, Task } from '../board.model';
 
 @Component({
   selector: 'app-board',
@@ -11,10 +13,39 @@ import { Board } from '../board.model';
 export class BoardComponent {
   @Input() board!: Board;
 
-  constructor(private boardService: BoardService) {}
-
   taskDrop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.board.tasks!, event.previousIndex, event.currentIndex);
     this.boardService.updateTasks(this.board.id!, this.board.tasks!);
   }
+
+  openDialog(task?: Task, idx?: number): void {
+    const newTask = { label: 'purple' };
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '500px',
+      data: task
+        ? { task: { ...task }, isNew: false, boardId: this.board.id, idx }
+        : { task: newTask, isNew: true }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        if (result.isNew) {
+          this.boardService.updateTasks(this.board.id!, [
+            ...this.board.tasks!,
+            result.task
+          ]);
+        } else {
+          const update = this.board.tasks;
+          update?.splice(result.idx, 1, result.task);
+          this.boardService.updateTasks(this.board.id!, this.board.tasks!);
+        }
+      }
+    });
+  }
+
+  handleDelete() {
+    this.boardService.deleteBoard(this.board.id!);
+  }
+
+  constructor(private boardService: BoardService, private dialog: MatDialog) {}
 }
