1f89344c2130bafdbd50212b8730884e467148c8
Vitor Pouza <vhnpouza@gmail.com>
2022-05-09 21:18:41 -0300

    board list, dialogs

--- src/app/kanban/board-list/board-list.component.ts
+++ src/app/kanban/board-list/board-list.component.ts
@@ -1,32 +1,51 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { BoardDialogComponent } from '../dialogs/board-dialog.component';
 import { Board } from '../board.model';
 import { BoardService } from '../board.service';
 
 @Component({
-  selector: 'app-board-list',
-  templateUrl: './board-list.component.html',
-  styleUrls: ['./board-list.component.scss']
+  selector: 'app-boards-list',
+  templateUrl: './boards-list.component.html',
+  styleUrls: ['./boards-list.component.scss']
 })
-export class BoardListComponent implements OnInit, OnDestroy {
+export class BoardsListComponent implements OnInit, OnDestroy {
+
   boards?: Board[];
   sub!: Subscription;
 
-  constructor(public boardService: BoardService) {}
+  constructor(public boardService: BoardService, public dialog: MatDialog) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.sub = this.boardService
       .getUserBoards()
-      .subscribe((boards: Board[]) => (this.boards = boards));
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
+      .subscribe(boards => (this.boards = boards));
   }
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.boards!, event.previousIndex, event.currentIndex);
     this.boardService.sortBoards(this.boards!);
+  }
+
+  openBoardDialog(): void {
+    const dialogRef = this.dialog.open(BoardDialogComponent, {
+      width: '400px',
+      data: {  }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.boardService.createBoard({
+          title: result,
+          priority: this.boards!.length
+        });
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
--- src/app/kanban/dialogs/board-dialog.component.ts
+++ src/app/kanban/dialogs/board-dialog.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-board-dialog',
-  template: ` <p>board-dialog works!</p> `,
+  template: `
+    <h1 mat-dialog-title>Board</h1>
+    <div mat-dialog-content>
+    <p>What shall we call this board?</p>
+      <mat-form-field>
+        <input placeholder="title" matInput [(ngModel)]="data.title" />
+      </mat-form-field>
+    </div>
+    <div mat-dialog-actions>
+      <button mat-button (click)="onNoClick()">Cancel</button>
+      <button mat-button [mat-dialog-close]="data.title" cdkFocusInitial>
+        Create
+      </button>
+    </div>
+  `,
   styles: []
 })
-export class BoardDialogComponent implements OnInit {
-  constructor() {}
+export class BoardDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<BoardDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
-  ngOnInit(): void {}
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
